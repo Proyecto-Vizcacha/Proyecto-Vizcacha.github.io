ac71d95aeb5df84a729c66572aac875c7b349131
Jinghao <jlin031@g.educaand.es>
2024-02-26 18:31:15 +0100

    index.js añade el div de los perros

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ fetch(ruta_info)
         for (perro of lista) {
             let info = perro.split("\r\n")
             
-            let cont_nombre = document.createElement("h1")
-            cont_nombre.className = "perro"
+            let cont_perro = document.createElement("div")
+            cont_perro.className = "perro"
+            
+            let cont_nombre = document.createElement("h2")
+            cont_nombre.textContent = info[0]
+            cont_perro.appendChild(cont_nombre)
 
-            //mascotas.appendChild(cont_nombre)
+            let cont_tamano = document.createElement("h3")
+            cont_tamano.textContent = info[1]
+            cont_perro.appendChild(cont_tamano)
+
+            if (info[2]) {
+                let cont_descripcion = document.createElement("p")
+                cont_descripcion.textContent = info[2]
+                cont_perro.appendChild(cont_descripcion)
+            }
+
+            mascotas.appendChild(cont_perro)
         }
     })
