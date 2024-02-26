d5a78d0a6f3fb2ba6715d98abd5681409c23c388
Jinghao <jlin031@g.educaand.es>
2024-02-26 18:30:57 +0100

    index.js añade el div de los perros

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const mascotas = document.querySelector("#mascotas")
 const ruta_info = "img/media_refugio/Lobitos/Info Dogos.txt"
 fetch(ruta_info)
     .then((resultado) => resultado.text())
     .then((texto) => {
-        const lista = texto.split("\n\n")
-        for(texto_lista of lista) {
-            console.log(texto_lista)
-            console.log("-------------")
+        const lista = texto.split("\r\n\r").map((n) =>n.trim()) 
+        for (perro of lista) {
+            let info = perro.split("\r\n")
+            
+            let cont_nombre = document.createElement("h1")
+            cont_nombre.className = "perro"
+
+            //mascotas.appendChild(cont_nombre)
         }
     })
