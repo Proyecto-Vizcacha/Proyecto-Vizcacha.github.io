9232d14a660c7632f20b78c9e76987146680ae63
Jinghao <jlin031@g.educaand.es>
2024-03-03 19:26:55 +0100

    la información de los perros se almacena en json ahora

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 const mascotas = document.querySelector("#mascotas")
-const ruta_info = "/img/media_refugio/Lobitos/Info Dogos.txt"
+const ruta_info = "/img/media_refugio/Lobitos/Info Dogos.json"
 fetch(ruta_info)
-    .then((resultado) => resultado.text())
-    .then((texto) => {
-        const lista = texto.split("\r\n\r").map((n) =>n.trim()) 
+    .then((resultado) => resultado.json())
+    .then((lista) => {
         for (perro of lista) {
-            let info = perro.split("\r\n")
-            
             let cont_perro = document.createElement("div")
             cont_perro.className = "perro"
+            let nombre = decodeURI(perro.name)
+            let tamano = decodeURI(perro.size)
+            let desc = decodeURI(perro.desc) || ""
             
             let cont_nombre = document.createElement("h2")
-            cont_nombre.textContent = info[0]
+            cont_nombre.textContent = nombre
             cont_perro.appendChild(cont_nombre)
 
             let cont_tamano = document.createElement("h3")
-            cont_tamano.textContent = info[1]
+            cont_tamano.textContent = tamano 
             cont_perro.appendChild(cont_tamano)
 
             let cont_imagen = document.createElement("img")
-            cont_imagen.src = "img/media_refugio/Lobitos/"+info[0]+".jpeg"
-            cont_imagen.alt = "imagen "+info[0]
+            cont_imagen.src = "img/media_refugio/Lobitos/"+nombre+".jpeg"
+            cont_imagen.alt = "imagen "+nombre
             cont_imagen.loading = "lazy"
             cont_imagen.decode() //Para decodificar la imágen en el momento en el que se carga y evitar que la página se pare durante el scroll
             cont_perro.appendChild(cont_imagen)
             cont_perro.addEventListener("click", function() {
-                let query = "nombre="+encodeURI(info[0])
-                alert("Adoptando "+info[0]+"...")
+                let query = "nombre="+encodeURI(nombre)
+                alert("Adoptando "+nombre+"...")
                 console.log("query_string: "+"\""+query+"\"")
             })
 
-            if (info[2]) {
-                let cont_descripcion = document.createElement("p")
-                cont_descripcion.textContent = info[2]
-                cont_perro.appendChild(cont_descripcion)
-            }
-
-
+            let cont_descripcion = document.createElement("p")
+            cont_descripcion.textContent =desc 
+            cont_perro.appendChild(cont_descripcion)
 
             mascotas.appendChild(cont_perro)
         }
